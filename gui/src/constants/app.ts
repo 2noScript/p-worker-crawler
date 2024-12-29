235cf8f40f40b08f0e6b5e725f1a4262808e3484
2noScript <thangmk7@gmail.com>
2024-12-29 21:25:17 +0700

    update: base router

---
+++ gui/src/constants/app.ts
@@ -0,0 +1,12 @@
+export const LAYOUTS = Object.freeze({
+  DEFAULT: "default",
+  NONE: "none",
+});
+
+export const URL = Object.freeze({
+  Home: "/",
+  Admin: "/admin",
+});
+
+
+export default null
--- gui/vite.config.ts
+++ gui/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "src/styles/common";
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      src: path.resolve("./src"),
+    },
+  },
 })
